0b936a60f2f221edd950486d5bfdc505b7eb4af6
Daniel Liao <danielliao22@gmail.com>
2020-04-12 18:15:04 -0700

    Scenes and Assets Implemented

---
+++ src/scenes/Play.js
@@ -0,0 +1,36 @@
+// When creating a scene, Phaser automatically calls init(), preload(), create(), update()
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    preload(){
+        // load images/title sprite
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create(){
+       // console.log(this);
+        //this.add.text(20, 20, "Rocket Patrol Play");
+
+        // palce tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0.0);
+
+        //white rectangle bnorders
+        this.add.rectangle(5,5,630,32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5,443,630,32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5,5,32,455, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(603,5,32,455, 0xFFFFFF).setOrigin(0, 0);
+        //green UI background
+        this.add.rectangle(37,42,566,64, 0x00FF00).setOrigin(0, 0);
+
+
+    }
+
+    update(){
+        // scroll starfield
+        this.starfield.tilePositionX -= 4;
+    }
+}
