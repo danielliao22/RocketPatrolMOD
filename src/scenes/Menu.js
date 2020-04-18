ad33947353e23b5f081b707af9ad16bb2e4b47c8
Daniel Liao <danielliao22@gmail.com>
2020-04-17 18:53:14 -0700

    New Titlescreen

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,37 +10,39 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.image('titleScreen', './assets/TITLESCREEN.png');
     }
 
     create(){
 
         // launch the next scene
         //this.scene.start("playScene");
+        this.titleScreen = this.add.tileSprite(0, 0, 640, 480, 'titleScreen').setOrigin(0.0);
 
         // menu display
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
+        // let menuConfig = {
+        //     fontFamily: 'Courier',
+        //     fontSize: '28px',
+        //     backgroundColor: '#F3B141',
+        //     color: '#843605',
+        //     align: 'right',
+        //     padding: {
+        //         top: 5,
+        //         bottom: 5,
+        //     },
+        //     fixedWidth: 0
+        // }
 
-        //show menu text
-        let centerX = game.config.width/2;
-        let centerY = game.config.height/2;
-        let textSpacer = 64;
+        // //show menu text
+        // let centerX = game.config.width/2;
+        // let centerY = game.config.height/2;
+        // let textSpacer = 64;
 
-        this.add.text(centerX, centerY- textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY, 'Use <--> arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = "#00FF00";
-        menuConfig.color = "#000";
-        this.add.text(centerX, centerY + textSpacer, 'Use <- for Easy or -> for Hard', menuConfig).setOrigin(0.5);
+        // this.add.text(centerX, centerY- textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        // this.add.text(centerX, centerY, 'Use <--> arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
+        // menuConfig.backgroundColor = "#00FF00";
+        // menuConfig.color = "#000";
+        // this.add.text(centerX, centerY + textSpacer, 'Use <- for Easy or -> for Hard', menuConfig).setOrigin(0.5);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -20,13 +20,13 @@ class Play extends Phaser.Scene {
         // palce tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0.0);
 
-        //white rectangle bnorders
-        this.add.rectangle(5,5,630,32, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(5,443,630,32, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(5,5,32,455, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(603,5,32,455, 0xFFFFFF).setOrigin(0, 0);
-        //green UI background
-        this.add.rectangle(37,42,566,64, 0x00FF00).setOrigin(0, 0);
+        // //white rectangle bnorders
+        // this.add.rectangle(5,5,630,32, 0xFFFFFF).setOrigin(0, 0);
+        // this.add.rectangle(5,443,630,32, 0xFFFFFF).setOrigin(0, 0);
+        // this.add.rectangle(5,5,32,455, 0xFFFFFF).setOrigin(0, 0);
+        // this.add.rectangle(603,5,32,455, 0xFFFFFF).setOrigin(0, 0);
+        // //green UI background
+        // this.add.rectangle(37,42,566,64, 0x00FF00).setOrigin(0, 0);
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, 435, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
